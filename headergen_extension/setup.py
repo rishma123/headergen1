03ad5be3c0a46e52a133de24fa5cc8c6b5c941e6
rishma123 <97422310+rishma123@users.noreply.github.com>
2024-11-23 14:06:27 +0100

    Updated changes with extension building

---
+++ headergen_extension/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='headergen_extension',
+    version='0.1.0',
+    description='A Jupyter Notebook extension',
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    package_data={
+        'headergen_extension': [
+            'static/main.js',
+            'extension.json'
+        ]
+    },
+    include_package_data=True,
+    install_requires=[
+        'notebook<7',
+        'jupyter_contrib_nbextensions'
+    ],
+    # entry_points={
+    #     'jupyter_nbextension': [
+    #         'headergen_extension = headergen_extension.main:load_ipython_extension'
+    #     ]
+    # },
+    # data_files=[  # Ensure this points to the right locations for JS and JSON
+    #     ('share/jupyter/nbextensions/headergen_extension', [
+    #         'headergen_extension/static/main.js',  # This is your main JavaScript file
+    #     ]),
+    #     ('etc/jupyter/nbconfig/notebook.d', [
+    #         'headergen_extension/extension.json',  # The config JSON file
+    #     ])
+    # ],
+    zip_safe=False,
+)
